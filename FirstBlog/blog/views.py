1394ea61013bcca503f5882e3fcf998bd0ebb555
olouie <olivia.louie@gmail.com>
2015-07-26 21:24:54 -0700

    first commit

---
+++ FirstBlog/blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render_to_response
+
+from FirstBlog.blog.models import posts
+
+def home(request):
+	entries = posts.objects.all()[:10]
+	return render_to_response('index.html', {'posts' : entries})
+
